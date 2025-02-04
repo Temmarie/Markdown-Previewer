3a59d8893f8ec9aab005fff6122b0f7661a87e02
Temmarie <temmar_whyte16@yahoo.com>
2025-02-04 08:07:31 +0100

    Add logo and other colors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import M5 from '../assets/M5.png';
 
 const Nav = ({ toggleTheme, isDarkMode }) => {
     return (
-        <nav className="p-4 flex justify-between items-center bg-gray-800 text-white">
-            <h1 className="text-2xl font-bold">Markdown Previewer</h1>
+        <nav className="p-4 flex justify-between items-center bg-[#20075A] text-white">
+            <a href="https://github.com/temmarie" className="flex items-center" target="_blank" rel="noopener noreferrer">
+                <img src={M5} alt="Logo" className="h-6 w-6 mr-2" />
+                <h1 className="text-2xl font-bold">Markkit</h1>
+            </a>
             <button 
                 onClick={toggleTheme} 
-                className="p-2 border rounded bg-emerald-200 text-gray-800"
+                className="p-2 border rounded bg-[#5C1AF4] text-white"
             >
                 {isDarkMode ? 'Light Mode' : 'Dark Mode'}
             </button>
